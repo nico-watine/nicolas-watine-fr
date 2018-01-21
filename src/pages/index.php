1cb43d5f00cf3b2f53c9000321e99e4cce04e955
Nico Watine <nico@studioncreations.com>
2018-01-21 15:05:01 -0600

    Set columns and remove specified height for div

--- src/pages/index.php
+++ src/pages/index.php
@@ -61,7 +61,7 @@ body-class: index
 <section class="biographyMonologue">
 	<div class="blurBackground absolute">
 		<div class="prose row relative">
-			<div class="columns">
+			<div class="large-6 large-push-3 columns">
 				<p><span class="h3 serif">For many years, I painted objects I could touch, objects close to me.</span> With them I created very architectural compositions. Still lives I was trying to animate with inert objects, daily objects assembled within their affinity and placed motionless on an horizontal setting, static, timeless, final.</p>
 				<p>Then, to get out of this immutable world, I opened my windows. The marvels of the Venetian palaces dazzled me. With my first canvases, I copied facades the way one would copy a common apple. Later, leaving the creativity of the Venetian architects, I composed my own facades using various existing elements but assembled differently to create imaginary palaces.</p>
 				<p>Since 2009, I have left both still lives and Venice. Off to the open sea. From a static world I left for a lively and dynamic one. The glasses of water from my still lives broke, the water from my carafes spread around and from my Venetian canals I went towards the sea. After the confined world of my still lives, immensity lays in front of me : the sea.</p>
