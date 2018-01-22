7f162bd34c9ecae666a625e8c501dd470adadd40
Nico Watine <nico@studioncreations.com>
2018-01-22 16:13:12 -0600

    Page setup with side bar and 3 artcards, inlined

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -8,3 +8,25 @@ og-image:
 body-id: gallery
 body-class: artGallery
 ---
+<div class="sideBar" style="width: 360px; float: left; background: #70848c;">
+	<h1 class="serif white">Venises</h1>
+</div>
+<div class="artCard-section" style="background: #F0F2F2; padding-left: 360px;">
+	<div class="row">
+		<div class="large-4 columns">
+			<div class="artCard boxShadow-1" style="height: 300px; background: #fff;">
+				&nbsp;
+			</div>
+		</div>
+		<div class="large-4 columns">
+			<div class="artCard boxShadow-1" style="height: 300px; background: #fff;">
+				&nbsp;
+			</div>
+		</div>
+		<div class="large-4 columns">
+			<div class="artCard boxShadow-1" style="height: 300px; background: #fff;">
+				&nbsp;
+			</div>
+		</div>
+	</div>
+</div>
