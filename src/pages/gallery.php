89315ae98acb3785f52866197aac0dab6dd7035e
Nico Watine <nico@studioncreations.com>
2018-01-22 16:52:36 -0600

    New .absolute-center class, apply to beta artCard

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -18,9 +18,7 @@ body-class: artGallery
 		<div class="large-4 columns">
 			<div class="artCard boxShadow-1 padding-XY" style="background: #fff; margin-bottom: 50px;">
 				<div class="artCard-image" style="position: relative; padding-bottom: 100%; margin-bottom: 15px;">
-					<div class="absolute text-center" style="width: 100%; height: 100%;">
-						<img style="max-height: 100%;" src="/img/beta/1.jpg" alt="">
-					</div>
+					<img class="absolute-center" style="max-height: 100%;" src="/img/beta/1.jpg" alt="">
 				</div>
 				<div class="artCard-prose">
 					
@@ -30,9 +28,7 @@ body-class: artGallery
 		<div class="large-4 columns">
 			<div class="artCard boxShadow-1 padding-XY" style="background: #fff; margin-bottom: 50px;">
 				<div class="artCard-image" style="position: relative; padding-bottom: 100%; margin-bottom: 15px;">
-					<div class="absolute" style="width: 100%; height: 100%;">
-						<img class="vertical-center" style="max-height: 100%;" src="/img/beta/3.jpg" alt="">
-					</div>
+					<img class="vertical-center" style="max-height: 100%;" src="/img/beta/3.jpg" alt="">
 				</div>
 				<div class="artCard-prose">
 					
