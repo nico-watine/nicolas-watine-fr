ced66dfa45c7c3b861a57bdd6ecc3931b3937779
Nico Watine <nico@studioncreations.com>
2018-01-22 16:42:24 -0600

    Make 3 artBox beta examples w different sized img

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -28,13 +28,27 @@ body-class: artGallery
 			</div>
 		</div>
 		<div class="large-4 columns">
-			<div class="artCard boxShadow-1" style="height: 300px; background: #fff; margin-bottom: 50px;">
-				&nbsp;
+			<div class="artCard boxShadow-1 padding-XY" style="background: #fff; margin-bottom: 50px;">
+				<div class="artCard-image" style="position: relative; padding-bottom: 100%; margin-bottom: 15px;">
+					<div class="absolute text-center" style="width: 100%; height: 100%;">
+						<img style="max-height: 100%;" src="/img/beta/3.jpg" alt="">
+					</div>
+				</div>
+				<div class="artCard-prose">
+					
+				</div>
 			</div>
 		</div>
 		<div class="large-4 columns">
-			<div class="artCard boxShadow-1" style="height: 300px; background: #fff; margin-bottom: 50px;">
-				&nbsp;
+			<div class="artCard boxShadow-1 padding-XY" style="background: #fff; margin-bottom: 50px;">
+				<div class="artCard-image" style="position: relative; padding-bottom: 100%; margin-bottom: 15px;">
+					<div class="absolute text-center" style="width: 100%; height: 100%;">
+						<img style="max-height: 100%;" src="/img/beta/5.jpg" alt="">
+					</div>
+				</div>
+				<div class="artCard-prose">
+					
+				</div>
 			</div>
 		</div>
 	</div>
