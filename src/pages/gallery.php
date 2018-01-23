380c7e792152db263d3471a524470aa0402fc02e
Nico Watine <nico@studioncreations.com>
2018-01-22 18:07:19 -0600

    Rearrangle cards so it isn’t a pattern for viewing

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -52,17 +52,6 @@ body-class: artGallery
 		<div class="medium-6 large-4 columns">
 			<div class="artCard" data-equalizer-watch>
 				<div class="artCard-image">
-					<img class="absolute-center" src="/img/beta/1.jpg" alt="">
-				</div>
-				<div class="artCard-prose">
-					<h4 class="title">Pieux rouges à Venise</h4>
-					<p class="dimensions">65 x 54 cm</p>
-				</div>
-			</div>
-		</div>
-		<div class="medium-6 large-4 columns">
-			<div class="artCard" data-equalizer-watch>
-				<div class="artCard-image">
 					<img class="absolute-center" src="/img/beta/3.jpg" alt="">
 				</div>
 				<div class="artCard-prose">
@@ -79,6 +68,17 @@ body-class: artGallery
 				<div class="artCard-prose">
 					<h4 class="title">Grand Venise gris</h4>
 					<p class="dimensions">116 x 89 cm</p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/1.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Pieux rouges à Venise</h4>
+					<p class="dimensions">65 x 54 cm</p>
 				</div>
 			</div>
 		</div>
