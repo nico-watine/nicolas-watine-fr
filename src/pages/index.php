127ae36b19675957d7895a0ca86ed89179f90ac4
Nico Watine <nico@studioncreations.com>
2018-02-15 14:14:12 -0600

    Copy/paste bio prose

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,15 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="biographyMonologue">
+	<div class="row">
+		<div class="columns">
+			<div class="prose">
+				<p><span class="h3 serif">For many years, I painted objects I could touch, objects close to me.</span> With them I created very architectural compositions. Still lives I was trying to animate with inert objects, daily objects assembled within their affinity and placed motionless on an horizontal setting, static, timeless, final.</p>
+				<p>Then, to get out of this immutable world, I opened my windows. The marvels of the Venetian palaces dazzled me. With my first canvases, I copied facades the way one would copy a common apple. Later, leaving the creativity of the Venetian architects, I composed my own facades using various existing elements but assembled differently to create imaginary palaces.</p>
+				<p>Since 2009, I have left both still lives and Venice. Off to the open sea. From a static world I left for a lively and dynamic one. The glasses of water from my still lives broke, the water from my carafes spread around and from my Venetian canals I went towards the sea. After the confined world of my still lives, immensity lays in front of me : the sea.</p>
+				<p class="lead">The sea is waves, transparencies, air flow, sea foam, boats. But it’s also sand beaches with sand yachting, horse riders, strollers, wooden or fabric cabins hustled by the wind...</p>
+			</div>
+		</div>
+	</div>
+</section>
