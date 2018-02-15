ea40c4492b063429a526f037fef6c2433f029db5
Nico Watine <nico@studioncreations.com>
2018-02-15 14:39:14 -0600

    Redirect img links

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,6 +8,31 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section id="dessins" class="bannerSection relative">
+	<div class="scrimContent flex text-center absolute">
+		<div class="prose absolute">
+			<h2 class="white line-height-1 margin-0 serif">Galeries des Dessins</h2>
+			<a href="" class="white h4 caps">Voir</a>
+		</div>
+	</div>
+	<div class="row expanded collapse flex">
+		<div class="oneFifth">
+			<img src="/img/drawings/sanguine/english/duck-3.jpg" alt="">
+		</div>
+		<div class="oneFifth relative">
+			<img class="absolute" src="/img/drawings/sanguine-charcoal-chalk/portrait-1.jpg" alt="">
+		</div>
+		<div class="oneFifth relative">
+			<img class="absolute" src="/img/drawings/sanguine/english/nude-2.jpg" alt="">	
+		</div>
+		<div class="oneFifth relative">
+			<img class="absolute" src="/img/drawings/black-color-pencil/portrait-1.jpg" alt="">
+		</div>
+		<div class="oneFifth relative">
+			<img class="absolute" src="/img/drawings/sanguine-charcoal-chalk/portrait-5.jpg" alt="">	
+		</div>
+	</div>
+</section>
 <section class="biographyMonologue padding-Y">
 	<div class="row">
 		<div class="columns">
