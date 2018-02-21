b6a632401def9d771093a507570dc51e8f5e57b4
Nico Watine <nico@studioncreations.com>
2018-02-20 18:03:23 -0600

    Got #seascapes @small good

--- src/pages/index.php
+++ src/pages/index.php
@@ -21,7 +21,7 @@ body-class: index
 				</div>
 				<div class="cornerDetail text-center">
 					<h4 class="caps margin-0"><strong>Gallery</strong></h4>
-					<span class="show-for-medium serif white"></span>
+					<span class="serif white"></span>
 				</div>
 			</div>
 		</div>
