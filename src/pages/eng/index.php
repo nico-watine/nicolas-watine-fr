9c665785867c0eebdb86f918cd616f52cbffa518
Nico Watine <nico@studioncreations.com>
2018-02-23 09:57:49 -0600

    Duplicate all layouts, pags and partials into eng

---
+++ src/pages/eng/index.php
@@ -0,0 +1,302 @@
+---
+url: /
+title:
+description:
+og-title:
+og-description:
+og-image:
+body-id: index
+body-class: index
+---
+<section class="galleryLinks" id="paintings" data-magellan-target="paintings" style="padding-top: 50px;">
+	<div class="row medium-collapse">
+		<div class="medium-11 columns end">
+			<div class="categoryRow" id="seascapes">
+				<div class="images text-center">
+					<img src="/img/paintings/seascapes/seascapes-32.jpg" style="max-height: 300px;" alt="">
+					<img class="show-for-large" src="/img/paintings/seascapes/seascapes-45.jpg" style="height: 300px; margin-left: 20px;" alt="">
+				</div>
+				<div class="prose">
+					<h2 class="serif text-center"><a href="">Seascapes</a></h2>
+				</div>
+				<div class="cornerDetail text-center">
+					<h4 class="caps margin-0"><strong>Gallery</strong></h4>
+					<span class="serif white"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="columns">
+			<div class="categoryRow" id="venice">
+				<div class="imgGroup-1 text-center">
+					<img class="primary" src="/img/paintings/venice/canvas/venice-canvas-3.jpg" alt="">
+					<img class="secondary" src="/img/paintings/venice/canvas/venice-canvas-2.jpg" alt="">
+				</div>
+				<div class="prose">
+					<h2 class="serif text-center"><a href="">Venice</a></h2>
+					<img class="show-for-medium" src="/img/assets/spacer.png" alt="">
+				</div>
+				<div class="cornerDetail text-center">
+					<h4 class="caps margin-0"><strong>Gallery</strong></h4>
+					<span class="serif white"></span>
+				</div>
+				<div class="imgGroup-2 absolute show-for-large">
+					<img class="show-for-xlarge" src="/img/paintings/venice/canvas/venice-canvas-2.jpg" alt="">
+					<img src="/img/paintings/venice/canvas/venice-canvas-7.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="xlarge-11 columns xlarge-push-1">
+			<div class="categoryRow" id="stillLife">
+				<div class="imgGroup-1 text-center">
+					<img class="primary" src="/img/paintings/still-life/still-life-21.jpg" alt="">
+					<img class="secondary" src="/img/paintings/still-life/still-life-4.jpg" alt="">
+				</div>
+				<div class="prose">
+					<h2 class="serif text-center"><a href="">Still Life</a></h2>
+				</div>
+				<div class="cornerDetail text-center">
+					<h4 class="caps margin-0"><strong>Gallery</strong></h4>
+					<span class="serif white"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="xlarge-11 columns end">
+			<div class="categoryRow" id="landscapes">
+				<div class="imgGroup-1">
+					<img class="primary" src="/img/paintings/landscapes/landscapes-4.jpg" alt="">
+					<img class="secondary" src="/img/paintings/landscapes/landscapes-1.jpg" alt="">
+				</div>
+				<div class="prose" style="text-align: center;">
+					<h2 class="serif"><a href="">Landscapes</a></h2>
+				</div>
+				<div class="cornerDetail text-center">
+					<h4 class="caps margin-0"><strong>Gallery</strong></h4>
+					<span class="serif white"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="drawings" id="drawings" data-magellan-target="drawings">
+	<div class="sectionContent row flex">
+		<div class="columns text-center completeCenter">
+			<h2 class="white serif margin-0 line-height-1">Drawings &amp; Sketches</h2>
+			<a href="" class="h4 white caps">View</a>
+		</div>
+	</div>
+</section>
+<br>
+<section class="biographyMonologue padding-Y">
+	<div class="row">
+		<div class="columns">
+			<div class="prose white">
+				<br><br>
+				<p><span class="h3 serif" style="line-height: 1.2;">For many years, I painted objects I could touch, objects close to me.</span> With them I created very architectural compositions. Still lives I was trying to animate with inert objects, daily objects assembled within their affinity and placed motionless on an horizontal setting, static, timeless, final.</p>
+				<p>Then, to get out of this immutable world, I opened my windows. The marvels of the Venetian palaces dazzled me. With my first canvases, I copied facades the way one would copy a common apple. Later, leaving the creativity of the Venetian architects, I composed my own facades using various existing elements but assembled differently to create imaginary palaces.</p>
+				<p>Since 2009, I have left both still lives and Venice. Off to the open sea. From a static world I left for a lively and dynamic one. The glasses of water from my still lives broke, the water from my carafes spread around and from my Venetian canals I went towards the sea. After the confined world of my still lives, immensity lays in front of me : the sea.</p>
+				<p class="lead">The sea is waves, transparencies, air flow, sea foam, boats. But it’s also sand beaches with sand yachting, horse riders, strollers, wooden or fabric cabins hustled by the wind...</p>
+				<br><br>
+			</div>
+		</div>
+	</div>
+</section>
+<br>
+<section class="sarton" id="sarton" data-magellan-target="sarton">
+	<div class="row collapse">
+		<div class="aspectRatio">
+			<div class="proseWrap">
+				<div class="prose padding-XY">
+					<h2 class="serif text-center white line-height-1 margin-0">Sarton Manor &amp; Gardens</h2>
+					<a href="" class="h4 white caps">Visit</a>
+				</div>
+				<img class="show-for-medium spacer" src="/img/assets/spacer.png">
+			</div>
+			<div class="row imgRow collapse absolute show-for-medium">
+				<div class="show-for-large large-4 columns">
+					<img src="/img/sarton/sarton-7.jpg" alt="">
+				</div>
+				<div class="medium-6 large-4 columns text-center medium-text-right">
+					<img src="/img/sarton/sarton-8.jpg" alt="">
+				</div>
+				<div class="medium-6 large-4 columns text-left">
+					<img src="/img/sarton/sarton-4.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<br>
+<br>
+<section class="featuredExhibitions" id="featuredExhibitions" data-magellan-target="featuredExhibitions">
+	<h2 class="serif text-center">Featured Exhibitions</h2>
+	<div class="exhibitionList white" style="padding: 50px 0;">
+		<div class="france" style="margin-bottom: 50px;">
+			<h3 style="margin-bottom: 16px;" class="caps text-center"><strong>In France</strong></h3>
+			<div class="row small-up-2 medium-up-3 large-up-4">
+				<div class="column column-block listing">
+					<p class="lead">Gallery Motte</p>
+					<p>Lille</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Vendôme, rue de la Paix</p>
+					<p>Paris</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Galerie l’Ermitage</p>
+					<p>Le Touquet</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Renaudot</p>
+					<p>Semur en Auxois</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Le Prieuré d’Airaines</p>
+					<p>Somme</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Amyot, Saint-Louis en l’Ile</p>
+					<p>Paris</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Dufour</p>
+					<p>Amiens</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Auriel</p>
+					<p>Toulouse</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Centre Culturel de la Somme</p>
+					<p>Somme</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery 26, Place des Vosges</p>
+					<p>Paris</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Hôtel de la Cloche</p>
+					<p>Dijon</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Art et Comparaison</p>
+					<p>Nantes</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Alain Daune, avenue Matignon</p>
+					<p>Paris</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Erath</p>
+					<p>Le Touquet</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Eglise de Fieffes</p>
+					<p>Somme</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery du Pharos</p>
+					<p>Marseille</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Centre Culturel de la Somme</p>
+					<p>Somme</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery World Fine Art</p>
+					<p>Cannes</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Hôtel Gantois</p>
+					<p>Lille</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Espace Prisme</p>
+					<p>Lille</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery 31</p>
+					<p>Lille</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery la Pléiade</p>
+					<p>Grenoble</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery des Molières</p>
+					<p>Miramas (13)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Manor of Sarton</p>
+					<p>Pas de Calais</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Neel</p>
+					<p>Paris &amp; Cannes</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Hôtel Carlton - La Croisette</p>
+					<p>Cannes</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Salon d’Automne</p>
+					<p>1er prix des Amis du Salon d’Automne</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Salon des Artistes Français</p>
+					<p>Médaille d’Argent</p>
+				</div>
+			</div>
+		</div>
+		<div class="international">
+			<h3 style="margin-bottom: 16px;" class="caps text-center"><strong>Internationally</strong></h3>
+			<div class="row small-up-2 medium-up-3 large-up-4">
+				<div class="column column-block listing">
+					<p class="lead">Gallery Marc Richard</p>
+					<p>Zurich (Suisse)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Parlement Européen</p>
+					<p>Bruxelles (Belgique)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Opéra Gallery</p>
+					<p>Singapour</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Art Yomiuri</p>
+					<p>Tokyo</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Tempéra</p>
+					<p>Bruxelles (Belgique)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Gallery Chérif Fine Art</p>
+					<p>Sidi Bous Saïd (Tunisie)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Nolan-Rankin Galleries</p>
+					<p>Houston (USA)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Galerie d’Haudrecy</p>
+					<p>Knokke le Zoute (Belgique)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Philips Galleries</p>
+					<p>Dallas (USA)</p>
+				</div>
+				<div class="column column-block listing">
+					<p class="lead">Newman &amp; Saunders Galleries</p>
+					<p>Philadelphie (USA)</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
