4903a9f006e43aa0286d2bcab2b386b179a3140a
Nico Watine <nico@studioncreations.com>
2018-02-23 10:37:00 -0600

    a translated

--- src/pages/index.php
+++ src/pages/index.php
@@ -113,7 +113,7 @@ body-class: index
 			<div class="proseWrap">
 				<div class="prose padding-XY">
 					<h2 class="serif text-center white line-height-1 margin-0">Jardin et Manoir de Sarton</h2>
-					<a href="" class="h4 white caps">Visit</a>
+					<a href="" class="h4 white caps">Visite</a>
 				</div>
 				<img class="show-for-medium spacer" src="/img/assets/spacer.png">
 			</div>
