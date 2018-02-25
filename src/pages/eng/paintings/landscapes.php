0eea6c8dfe447800804265f8033ed18e69360079
Nico Watine <nico@studioncreations.com>
2018-02-25 15:45:08 -0600

    Simple top level nav marked up

--- src/pages/eng/paintings/landscapes.php
+++ src/pages/eng/paintings/landscapes.php
@@ -6,6 +6,17 @@ og-title:
 og-description:
 og-image:
 body-id: landscapes
-body-class: artGallery
+body-class: galleryPage
 layout: eng-default
 ---
+<div class="row expanded">
+	<div class="columns">
+		<ul class="menu">
+			<li><h1 class="h2 white serif padding-X">Landscapes</h1></li>
+			<li class="h4 caps"><a href="">Seascapes</a></li>
+			<li class="h4 caps"><a href="">Venice</a></li>
+			<li class="h4 caps"><a href="">Still Life</a></li>
+			<li class="h4 caps"><a href="">Landscapes</a></li>
+		</ul>
+	</div>
+</div>
