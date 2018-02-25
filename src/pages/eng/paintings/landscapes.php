83e81a1c2b52bd60d1576c9e86c8d7c53b2e4758
Nico Watine <nico@studioncreations.com>
2018-02-25 15:46:11 -0600

    Tweak .columns to nav for semantics

--- src/pages/eng/paintings/landscapes.php
+++ src/pages/eng/paintings/landscapes.php
@@ -10,7 +10,7 @@ body-class: galleryPage
 layout: eng-default
 ---
 <div class="row expanded">
-	<div class="columns">
+	<nav class="columns">
 		<ul class="menu">
 			<li><h1 class="h2 white serif padding-X">Landscapes</h1></li>
 			<li class="h4 caps"><a href="">Seascapes</a></li>
@@ -18,5 +18,5 @@ layout: eng-default
 			<li class="h4 caps"><a href="">Still Life</a></li>
 			<li class="h4 caps"><a href="">Landscapes</a></li>
 		</ul>
-	</div>
+	</nav>
 </div>
