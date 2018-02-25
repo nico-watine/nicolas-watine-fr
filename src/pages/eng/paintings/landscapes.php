7ee87cfd4e6af63fb04e0e392e9bbead6cef98d9
Nico Watine <nico@studioncreations.com>
2018-02-25 15:28:35 -0600

    Duplicate contents from old gallery page

--- src/pages/eng/paintings/landscapes.php
+++ src/pages/eng/paintings/landscapes.php
@@ -9,3 +9,86 @@ body-id: landscapes
 body-class: artGallery
 layout: eng-default
 ---
+<div class="sideBar">
+	<h1 class="galleryTitle serif white padding-X margin-0">Sanguine, Charcoal &amp; Chalk</h1>
+</div>
+<nav class="galleryNav padding-Y">
+	<ul class="menu">
+		<li class="h4 caps"><a href="">Natures Mortes</a></li>
+		<li class="h4 caps"><a href="">Paysages</a></li>
+		<li class="h4 caps"><a href="">Marines</a></li>
+	</ul>
+</nav>
+<div class="artworkGrid">
+	<br>
+	<br>
+	<div class="row" data-equalizer data-equalize-on="medium" data-equalize-by-row="true">
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/1.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Pieux rouges à Venise Pieux rouges à Venise Pieux rouges à Venise</h4>
+					<p class="dimensions">65 x 54 cm</p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/3.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Volets verts à Venise</h4>
+					<p class="dimensions">41 x 33 cm</p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/5.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Grand Venise gris</h4>
+					<p class="dimensions">116 x 89 cm</p>
+				</div>
+			</div>
+		</div>
+		<!-- 2nd row :: -->
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/3.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Volets verts à Venise</h4>
+					<p class="dimensions">41 x 33 cm</p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/5.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Grand Venise gris</h4>
+					<p class="dimensions">116 x 89 cm</p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 large-4 columns">
+			<div class="artCard" data-equalizer-watch>
+				<div class="artCard-image">
+					<img class="absolute-center" src="/img/beta/1.jpg" alt="">
+				</div>
+				<div class="artCard-prose">
+					<h4 class="title">Pieux rouges à Venise</h4>
+					<p class="dimensions">65 x 54 cm</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
