a1dc0be7da98def654f10d2b32885486a4e7f0d4
Nico Watine <nico@studioncreations.com>
2018-02-26 17:54:17 -0600

    Update nav for page

--- src/pages/eng/paintings/still-life.php
+++ src/pages/eng/paintings/still-life.php
@@ -11,17 +11,17 @@ layout: eng-default
 ---
 <div class="row galleryNav">
 	<div class="columns">
-		<h1 class="h2 white serif margin-0">Landscapes</h1>
+		<h1 class="h2 white serif margin-0">Still Life</h1>
 		<div class="title-bar" data-responsive-toggle="categoryMenu" data-hide-for="medium">
 			<button class="menu-icon" type="button" data-toggle="categoryMenu"></button>
 			<div class="title-bar-title">Category Menu</div>
 		</div>
 		<nav class="categoryNav" id="categoryMenu">
 			<ul class="vertical medium-horizontal menu">
-				<li class="caps"><a href="">Seascapes</a></li>
-				<li class="caps"><a href="">Venice</a></li>
-				<li class="caps"><a href="">Still Life</a></li>
-				<li class="caps"><a class="active" href="#0">Landscapes</a></li>
+				<li class="caps"><a href="/eng/paintings/seascapes">Seascapes</a></li>
+				<li class="caps"><a href="/eng/paintings/venice">Venice</a></li>
+				<li class="caps"><a class="active" href="/eng/paintings/still-life">Still Life</a></li>
+				<li class="caps"><a href="/eng/paintings/landscapes">Landscapes</a></li>
 			</ul>
 		</nav>
 	</div>
