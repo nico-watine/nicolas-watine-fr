dab5bb4cd7c360f071e0e2c5aef9e1b224673175
Nico Watine <nico@studioncreations.com>
2018-02-27 13:48:36 -0600

    Insert all portrait artcards

--- src/pages/eng/drawings.php
+++ src/pages/eng/drawings.php
@@ -27,6 +27,141 @@ layout: eng-drawings
 </div>
 <div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" id="sanguine-charcoal-chalk">
 	<h2 class="subCategory-heading white serif text-center">Sanguine, Charcoal &amp; Chalk</h2>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-1.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-1.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-2.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-2.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-3.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-3.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-4.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-4.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-5.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-5.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-6.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-6.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-7.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-7.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-8.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-8.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-9.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-9.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-10.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-10.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-11.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-11.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-12.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-12.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-13.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-13.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-14.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-14.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="artCard">
+			<div class="artCard-image">
+				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-15.jpg">
+					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-15.jpg" alt="Artist Nicolas Watine Portrait Sketch">
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
 <hr>
 <div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" id="sanguine">
