7d4ef8cbc0d717a0fd9f87320e06654fa95fc6bd
Nico Watine <nico@studioncreations.com>
2018-02-27 15:40:50 -0600

    Empty the category nav for sarton

--- src/pages/eng/sarton.php
+++ src/pages/eng/sarton.php
@@ -13,24 +13,23 @@ layout: eng-sarton
 	<div class="sticky" data-sticky-on="small" data-sticky data-top-anchor="350" data-margin-top="0" style="width: 100%;">
 		<div class="row galleryNav">
 			<div class="columns">
-				<h1 class="galleryTitle white serif margin-0">Drawings</h1>
-				<div class="title-bar" data-responsive-toggle="categoryMenu" data-hide-for="medium">
+				<h1 class="galleryTitle white serif margin-0">Sarton</h1>
+				<!-- <div class="title-bar" data-responsive-toggle="categoryMenu" data-hide-for="medium">
 					<button class="menu-icon" type="button" data-toggle="categoryMenu"></button>
 					<div class="title-bar-title">Category Menu</div>
-				</div>
-				<nav class="categoryNav" id="categoryMenu">
+				</div> -->
+				<!-- <nav class="categoryNav" id="categoryMenu">
 					<ul class="vertical medium-horizontal menu" data-magellan data-deep-linking="true">
 						<li class="caps"><a href="#sanguine-charcoal-chalk">Sanguine, Charcoal &amp; Chalk</a></li>
 						<li class="caps"><a href="#sanguine">Sanguine</a></li>
 						<li class="caps"><a href="#black-or-color-pencil">Black or Color Pencil</a></li>
 					</ul>
-				</nav>
+				</nav> -->
 			</div>
 		</div>
 	</div>
 </div>
 <div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" data-magellan-target="sanguine-charcoal-chalk" id="sanguine-charcoal-chalk">
-	<h2 class="subCategory-heading white serif text-center">Sanguine, Charcoal &amp; Chalk</h2>
 	<div class="column">
 		<div class="artCard">
 			<div class="artCard-image">
@@ -162,113 +161,6 @@ layout: eng-sarton
 			<div class="artCard-image">
 				<a href="/img/drawings/sanguine-charcoal-chalk/portrait-15.jpg">
 					<img class="absolute-center" src="/img/drawings/sanguine-charcoal-chalk/portrait-15.jpg" alt="Artist Nicolas Watine Portrait Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-</div>
-<hr>
-<div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" data-magellan-target="sanguine" id="sanguine">
-	<h2 class="subCategory-heading white serif text-center">Sanguine</h2>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/sanguine/duck-2.jpg">
-					<img class="absolute-center" src="/img/drawings/sanguine/duck-2.jpg" alt="Artist Nicolas Watine Sanguine Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/sanguine/duck-1.jpg">
-					<img class="absolute-center" src="/img/drawings/sanguine/duck-1.jpg" alt="Artist Nicolas Watine Sanguine Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/sanguine/nude-1.jpg">
-					<img class="absolute-center" src="/img/drawings/sanguine/nude-1.jpg" alt="Artist Nicolas Watine Sanguine Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/sanguine/rabbit-1.jpg">
-					<img class="absolute-center" src="/img/drawings/sanguine/rabbit-1.jpg" alt="Artist Nicolas Watine Sanguine Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/sanguine/nude-2.jpg">
-					<img class="absolute-center" src="/img/drawings/sanguine/nude-2.jpg" alt="Artist Nicolas Watine Sanguine Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/sanguine/duck-3.jpg">
-					<img class="absolute-center" src="/img/drawings/sanguine/duck-3.jpg" alt="Artist Nicolas Watine Sanguine Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-</div>
-<hr>
-<div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" data-magellan-target="black-or-color-pencil" id="black-or-color-pencil">
-	<h2 class="subCategory-heading white serif text-center">Black or Color Pencil</h2>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/black-color-pencil/nude-1.jpg">
-					<img class="absolute-center" src="/img/drawings/black-color-pencil/nude-1.jpg" alt="Artist Nicolas Watine Pencil Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/black-color-pencil/portrait-1.jpg">
-					<img class="absolute-center" src="/img/drawings/black-color-pencil/portrait-1.jpg" alt="Artist Nicolas Watine Pencil Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/black-color-pencil/tree-1.jpg">
-					<img class="absolute-center" src="/img/drawings/black-color-pencil/tree-1.jpg" alt="Artist Nicolas Watine Pencil Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/black-color-pencil/portrait-2.jpg">
-					<img class="absolute-center" src="/img/drawings/black-color-pencil/portrait-2.jpg" alt="Artist Nicolas Watine Pencil Sketch">
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="column">
-		<div class="artCard">
-			<div class="artCard-image">
-				<a href="/img/drawings/black-color-pencil/tree-2.jpg">
-					<img class="absolute-center" src="/img/drawings/black-color-pencil/tree-2.jpg" alt="Artist Nicolas Watine Pencil Sketch">
 				</a>
 			</div>
 		</div>
