ca320613d11e59b37869cffbdc6f2f2e6ecabdc4
Nico Watine <nico@studioncreations.com>
2018-02-27 13:56:39 -0600

    Magellan scroll set up for 1st section

--- src/pages/eng/drawings.php
+++ src/pages/eng/drawings.php
@@ -17,15 +17,15 @@ layout: eng-drawings
 			<div class="title-bar-title">Category Menu</div>
 		</div>
 		<nav class="categoryNav" id="categoryMenu">
-			<ul class="vertical medium-horizontal menu">
-				<li class="caps"><a href="/eng/paintings/seascapes">Sanguine, Charcoal &amp; Chalk</a></li>
+			<ul class="vertical medium-horizontal menu" data-magellan>
+				<li class="caps"><a href="#sanguine-charcoal-chalk">Sanguine, Charcoal &amp; Chalk</a></li>
 				<li class="caps"><a href="/eng/paintings/venice">Sanguine</a></li>
 				<li class="caps"><a href="/eng/paintings/still-life">Black or Color Pencil</a></li>
 			</ul>
 		</nav>
 	</div>
 </div>
-<div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" id="sanguine-charcoal-chalk">
+<div class="row gallery artCard-grid small-up-1 medium-up-2 large-up-4" data-magellan-target="sanguine-charcoal-chalk" id="sanguine-charcoal-chalk">
 	<h2 class="subCategory-heading white serif text-center">Sanguine, Charcoal &amp; Chalk</h2>
 	<div class="column">
 		<div class="artCard">
